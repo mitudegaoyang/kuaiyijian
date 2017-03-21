59f0e20d69d2216290e57992b05fc640b6e72aaf
gaotianyang <13683265113@163.com>
2017-03-21 15:59:06 +0800

    动态添加

--- js/base.js
+++ js/base.js
@@ -2,8 +2,8 @@
  * Created by gaotianyang(13683265113@163.com) on 2017/3/21.
  **/
 var myArticle=[
-    { "url":"pages/demo01.html" , "img":"" , "title":"demo01" , "text":"213" },
-    { "url":"pages/demo01.html" , "img":"" , "title":"demo02" , "text":"213" }];
+    { "url":"pages/contents_folder/article/demo01.html" , "img":"" , "title":"demo01" , "text":"213" },
+    { "url":"pages/contents_folder/article/demo01.html" , "img":"" , "title":"demo02" , "text":"213" }];
 var myProject=[
     { "url":"pages/contents_folder/resume/index.html" , "img":"img/images/mypic1.jpg" , "title":"电子简历" , "text":"213" },
     { "url":"pages/contents_folder/3Dmove/share.html" , "img":"img/images/mypic2.jpg" , "title":"店庆抓小鸡" , "text":"213" },
