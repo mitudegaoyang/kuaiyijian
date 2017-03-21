b8dd63af3166d58b918f35fa4f34e992566ee5fb
gaotianyang <13683265113@163.com>
2017-03-21 15:34:18 +0800

    动态添加

--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
 /**
  * Created by gaotianyang(13683265113@163.com) on 2017/3/21.
  **/
-var myArticle=[
-        { "url":"pages/demo01.html" , "img":"" , "title":"demo01" , "text":"213" },
-        { "url":"pages/demo01.html" , "img":"" , "title":"demo02" , "text":"213" }];
-var myProject=[
-        { "url":"pages/contents_folder/resume/index.html" , "img":"img/images/mypic1.jpg" , "title":"电子简历" , "text":"213" },
-        { "url":"pages/contents_folder/3Dmove/share.html" , "img":"img/images/mypic2.jpg" , "title":"店庆抓小鸡" , "text":"213" },
-        { "url":"pages/contents_folder/calc/calc.html" , "img":"img/images/mypic3.jpg" , "title":"移动端计算器" , "text":"213" },
-        { "url":"pages/contents_folder/yumaojinfu/maoyuMoney.html" , "img":"img/images/mypic4.jpg" , "title":"鱼猫金服" , "text":"213" },
-        { "url":"pages/contents_folder/yumaonianhui/index.html" , "img":"img/images/mypic5.jpg" , "title":"鱼猫年会" , "text":"213" }];
 //动态加载文章内容
 (function(){
-        $(".my-article .row").html("");
+    $(".my-article .row").html("");
+    myArticle.length=(myArticle.length>10)?10:myArticle.length;
         for(var i=0;i<myArticle.length;i++){
             var img=(myArticle[i].img=="")?"":"<img src=`+myArticle[i].img+` alt=‘...’>";
             $(".my-article .row").append(`
@@ -32,6 +24,7 @@ var myProject=[
 //动态加载项目内容
 (function(){
     $(".my-project").html("<p>最新项目</p>");
+    myProject.length=(myProject.length>10)?10:myProject.length;
     for(var i=0;i<myProject.length;i++){
         $(".my-project").append(`
             <div class="media">
