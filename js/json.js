7b5c6644e30f9d365f7ccb519668aba54f1053ab
gaotianyang <13683265113@163.com>
2017-03-22 14:16:38 +0800

    json

--- js/json.js
+++ js/json.js
@@ -2,10 +2,22 @@
  * Created by gaotianyang(13683265113@163.com) on 2017/3/21.
  **/
 var myArticle=[
+    { "url":"pages/contents_folder/article/art_2017_3_16_picture.html" , "img":"" , "title":"图片资源网站" , "text":"文章简介" },
     { "url":"pages/contents_folder/article/art_2017_3_16_cssLoading.html" , "img":"" , "title":"CSS解决loading效果" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_14_fontAwesome.html" , "img":"" , "title":"一套绝佳的图标字体库和CSS框架:Font Awesome" , "text":"文章简介" },
     { "url":"pages/contents_folder/article/art_2017_3_13_less&sass.html" , "img":"" , "title":"LESS Sass 编译工具Koala介绍" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_12_mobileWeCharMusic.html" , "img":"" , "title":"解决ios下的微信页面背景音乐无法自动播放问题" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_12_mobileHorizontalScreen.html" , "img":"" , "title":"移动端页面通过前端方法禁止ios页面横屏方法" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_12_Ajax.html" , "img":"" , "title":"关于Jquery封装的AJAX基本使用方法" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_11_css3D.html" , "img":"" , "title":"有关CSS3D的插件使用介绍" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_9_mobile.html" , "img":"" , "title":"移动端插件" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_8_ico.html" , "img":"" , "title":"关于ico图标不显示解决办法" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_8_responseSpeed.html" , "img":"" , "title":"提升页面响应速度" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_7_vim.html" , "img":"" , "title":"VIM常用代码汇总" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_7_linuxShell.html" , "img":"" , "title":"Linux Shell常用shell命令" , "text":"文章简介" },
+    { "url":"pages/contents_folder/article/art_2017_3_6_markdown.html" , "img":"" , "title":"Markdown 标记示例" , "text":"文章简介" },
     { "url":"pages/contents_folder/article/art_2017_3_6_repositories.html" , "img":"" , "title":"Git 使用技巧 01 删除repositories" , "text":"文章简介" },
-    { "url":"pages/contents_folder/article/demo01.html" , "img":"" , "title":"demo02" , "text":"文章简介" }];
+    { "url":"pages/contents_folder/article/demo01.html" , "img":"" , "title":"demo01" , "text":"文章简介" }];
 var myProject=[
     { "url":"pages/contents_folder/resume/index.html" , "img":"img/images/mypic1.jpg" , "title":"电子简历" , "text":"项目简介" },
     { "url":"pages/contents_folder/3Dmove/share.html" , "img":"img/images/mypic2.jpg" , "title":"店庆抓小鸡" , "text":"项目简介" },
