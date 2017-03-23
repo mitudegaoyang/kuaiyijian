81c116df894ead878c428770714d4bfe08b6810a
gaotianyang <13683265113@163.com>
2017-03-23 15:43:17 +0800

    init js

--- pages/contents_folder/GoldenChicken/js/index.js
+++ pages/contents_folder/GoldenChicken/js/index.js
@@ -2,5 +2,8 @@
  * Created by gaotianyang(13683265113@163.com) on 2017/3/23.
  **/
 var game={
-
+    init:function(){
+        $('.chicken-gift').css({display:"none"});
+        $('.chicken-box').css({transform:"rotate(0deg)"});
+    }
 };
