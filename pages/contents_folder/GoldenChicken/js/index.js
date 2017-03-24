689c9912b3e2306134e166726b20bb6809217f86
gaotianyang <13683265113@163.com>
2017-03-24 11:46:11 +0800

    1.0 游戏主体

--- pages/contents_folder/GoldenChicken/js/index.js
+++ pages/contents_folder/GoldenChicken/js/index.js
@@ -4,12 +4,17 @@
 var game={
     run: true,          //游戏是否开始
     deg: 0,             //小鸡角度
-    regulation:true,    //小鸡转动方向
+    coe: 0,             //系数
+    listenGo: false,     //是否监听
+    regulation: true,    //小鸡转动方向 true左 false右
+    waggleGo: true,     //是否晃动
     waggleMin: -10,     //小鸡晃动最小角度
     waggleMax: 10,      //小鸡晃动最大角度
     //初始化
     init:function(){
         $('.chicken-gift').css({display:"block"});
+        $('.dizzyLeftEye').css({display:"none"});
+        $('.dizzyRightEye').css({display:"none"});
         $('.chicken-box').css({transform:"rotate(0deg)"});
         game.time(16);
         $('#stage').click(function(){
@@ -19,6 +24,12 @@ var game={
     start:function(){
         console.log("start");
         $('.chicken-gift').css({display:"none"});
+        $('.dizzyLeftEye').css({display:"none"});
+        $('.dizzyRightEye').css({display:"none"});
+        this.waggleGo = false;
+        this.listenGo = true;
+        this.deg = 0;
+        this.listen();
     },
     //定时器
     time:function (millisec) {
@@ -28,7 +39,6 @@ var game={
                 if(game.run){
                     game.waggle(game.waggleMin,game.waggleMax);
                     game.rotate(game.deg);
-                    console.log(game.deg);
                     T();
                 }else{
                     console.log("停止旋转");
@@ -42,16 +52,46 @@ var game={
     },
     //起始小鸡晃动
     waggle:function(min,max){
-        if(game.regulation){
-            game.deg+=0.5;
-            if(game.deg>max){
-                game.regulation=false;
+        if(this.waggleGo){
+            if(game.regulation){
+                game.deg+=0.5;
+                if(game.deg>max){
+                    game.regulation=false;
+                }
+            }else{
+                game.deg-=0.5;
+                if(game.deg<min){
+                    game.regulation=true;
+                }
             }
-        }else{
-            game.deg-=0.5;
-            if(game.deg<min){
-                game.regulation=true;
+        }
+    },
+    //小鸡移动
+    move:function(){
+        if(game.listenGo){
+            this.deg += 0.5 + game.coe;
+            $('.logo').text(this.deg);
+            if(this.deg >= 90 || this.deg <= -90){
+                $('.dizzyLeftEye').css({display:"block"});
+                $('.dizzyRightEye').css({display:"block"});
+                $('.logo').text('游戏结束!');
+                game.coe = 0;
+                game.deg = 0;
+                game.listenGo = false;
             }
+        }
+    },
+    //监听手机旋转
+    listen:function(){
+        if(game.listenGo){
+            window.addEventListener('deviceorientation', function(e){
+                //console.log('absolute: ' + e.absolute);
+                //console.log('alpha: ' + e.alpha);
+                //console.log('beta: ' + e.beta);
+                console.log('gamma: ' + e.gamma);
+                game.coe = Math.floor(e.gamma * 10)/100;
+                game.move();
+            });
         }
     }
 };
