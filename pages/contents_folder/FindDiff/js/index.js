e3512209dc170b65590cdda8b74234280ad00d9d
mitudegaoyang <13683265113@163.com>
2017-03-29 20:44:45 +0800

    删除多余console

--- pages/contents_folder/FindDiff/js/index.js
+++ pages/contents_folder/FindDiff/js/index.js
@@ -9,7 +9,7 @@ var game={
         game.skip('share');
         $('.lv span').text(game.lv);
         $('.startBtn').click(function(){
-            game.time = 16;
+            game.time = 30;
             game.lv = 1;
             game.skip('game');
             game.countDown(1000);
@@ -30,7 +30,6 @@ var game={
     monitor:function(){
         $('.abrus').click(function(){
             game.lv++;
-            console.log(game.lv);
             $('.lv span').text(game.lv);
             if(game.lv <= 8){
                 $('#stage').removeClass();
@@ -44,29 +43,23 @@ var game={
         switch(game.lv){
             case 1:
                 game.draw(4);
-                console.log("2x2");
                 break;
             case 2:
                 game.draw(9);
-                console.log("3x3");
                 break;
             case 3:
                 game.draw(16);
-                console.log("4x4");
                 break;
             case 4:
             case 5:
                 game.draw(25);
-                console.log("5x5");
                 break;
             case 6:
             case 7:
                 game.draw(36);
-                console.log("6x6");
             break;
             default:
                 game.draw(49);
-                console.log("7x7");
             break;
         }
     },
